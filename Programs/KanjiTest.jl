79dac0ad98aad772e6ee79698e46a4a15dca7ff1
Luis Felipe Gómez López <L.Felipe_Gomez@ciencias.unam.mx>
2019-06-12 23:11:56 -0500

    ArgParse Completed

--- Programs/KanjiTest.jl
+++ Programs/KanjiTest.jl
@@ -1,7 +1,39 @@
 using DataFrames
 using CSVFiles
+using ArgParse
 
-kanjis = DataFrame(load("KanjisN4.csv"))
+
+#----------START ARGPARSE----------
+
+function ParseCommandline()
+    ArgumentsSettings = ArgParseSettings()
+    @add_arg_table ArgumentsSettings begin
+        "--folder_path", "-p"
+            help = "Path to the folder where the csv are"
+            arg_type = String
+            default = "../Datasets/KanjisNs/"
+        "--level", "-N"
+            help = "Level (N#) to be studied"
+            arg_type = Int64
+            default = 4
+    end
+    return parse_args(ArgumentsSettings)
+end
+
+parsed_args = ParseCommandline()
+
+function main(parsed_args)
+    println("Parsed args:")
+    for (arg,val) in parsed_args
+        println(" $arg => $val")
+    end
+end
+
+main(parsed_args)
+
+#----------END ARGPARSE-------------
+
+#----------START DEFINITIONS---------
 
 function KanjiTest(dataframe)
     long = size(dataframe)[1]
@@ -49,6 +81,12 @@ function KanjiMemorize(dataframe)
     return
 end
 
+#------------END DEFINITIONS------------
+
+#------------START EXECUTION------------
+
+path = parsed_args["folder_path"] * "KanjisN" * string(parsed_args["level"]) * ".csv"
+kanjis = DataFrame(load(path))
 KanjiMemorize(kanjis)
 
-#TODO Add ArgsParse
+#------------END EXECUTION---------------
