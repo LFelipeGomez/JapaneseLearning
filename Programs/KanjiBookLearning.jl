1a936a40d84a88a754f0dcc37f919928337737a4
Luis Felipe Gomez Lopez <L.Felipe_Gomez@ciencias.unam.mx>
2019-06-20 23:53:27 -0500

    Print BookIndex

--- Programs/KanjiBookLearning.jl
+++ Programs/KanjiBookLearning.jl
@@ -32,7 +32,7 @@ parsed_args = ParseCommandline()
 function main(parsed_args)
     println("Parsed args:")
     for (arg,val) in parsed_args
-        println(" $arg => $val")
+        println("$arg => $val")
     end
 end
 
@@ -44,19 +44,19 @@ main(parsed_args)
 
 function KanjiBookLearning(start_chapter=1,last_chapter=1, time=5, path="./KanjiBook")
     chapters = collect(start_chapter:last_chapter)
-    dataframe = DataFrame(español = String[])
+    dataframe = DataFrame(español = String[], BookIndex = Int64[])
     for chapter in chapters
         chapter_number = string(chapter)
         chapter_name = "KanjiBookPalabras" * chapter_number * ".csv"
-        palabras = DataFrame(load(path*"/" * chapter_name))
-        append!(dataframe[:español], palabras[:español])
+        df_palabras = DataFrame(load(path*"/" * chapter_name))
+        append!(dataframe, df_palabras)
     end
     long = size(dataframe)[1]
     index = collect(1:long)
     while length(index) != 0
         random = rand(index)
         filter!(e -> e≠random, index)
-        println(string(random)*" "*dataframe[random,:español])
+        println(string(dataframe[random,:BookIndex])*" "*dataframe[random,:español])
         sleep(time)
     end
 end
